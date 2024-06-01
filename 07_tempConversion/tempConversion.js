0880b35cccb6938c18d39f34254e3430f570c113
hanadmse <hanadmse@gmail.com>
2024-06-01 16:51:25 -0500

    Exercise 07 & 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (5/9) * (fahrenheit - 32);
+  if (!Number.isInteger(celsius)) {
+    return parseFloat(celsius.toFixed(1));
+  }
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = celsius * (9/5) + 32;
+  if (!Number.isInteger(fahrenheit)) {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
+  return fahrenheit;
 };
 
 // Do not edit below this line
