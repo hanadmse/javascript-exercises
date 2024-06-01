41ed0384f6f148fe278d916826279042cdf48067
hanadmse <hanadmse@gmail.com>
2024-06-01 11:30:48 -0500

    Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, param1, param2, param3, param4) {
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === param1 || array[i] === param2 || array[i] === param3 || array[i] === param4) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
