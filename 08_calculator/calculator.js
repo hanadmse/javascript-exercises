efe7039bc8e51d62ceff5a127784b9353a8bc8a1
hanadmse <hanadmse@gmail.com>
2024-06-01 16:50:49 -0500

    Exercise 07 & 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  array.forEach(element => {
+    product *= element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorial = 1
+  for (i = number; i > 1; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
