c0089df6dbcd20efe27751d8d4b751b2b909ba8b
hanadmse <hanadmse@gmail.com>
2024-06-01 12:36:42 -0500

    Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(param1, param2) {
+    let sum = 0;
+    let smaller = 0;
+    let larger = 0;
+    if (typeof param1 !== "number" || typeof param2 !== "number" || param1 < 0 || param2 < 0) {
+        return "ERROR";
+    }
+    if (param1 < param2) {
+        smaller = param1;
+        larger = param2;
+    }
+    else {
+        smaller = param2;
+        larger = param1;
+    }
+    for (i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
