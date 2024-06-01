2d3ad3ded12365196d3263e0b0d4cfe2dcea6467
hanadmse <hanadmse@gmail.com>
2024-05-31 20:01:53 -0500

    Exercise 02 Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return 'ERROR';
+    }
+    repeatedString = "";
+    for (i = 0; i < number; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
